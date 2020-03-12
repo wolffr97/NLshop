3ddc61cb773fbdf57bf011465d4cb108d3d15b26
NghiaLe <lenghia1098@gmail.com>
2020-03-12 21:38:55 +0700

    Data class adding

---
+++ NLshop.Data/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using NLshop.Data.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLshop.Data
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        NlShopDbContext dbContext;
+
+        public NlShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new NlShopDbContext());
+        }
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ NLshop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLshop.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        private bool isDispose;
+        ~Disposable()
+        {
+
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if(!isDispose && disposing)
+            {
+                DisposeCore();
+            }
+            isDispose = true;
+        }
+        protected virtual void DisposeCore()
+        {
+
+        }
+        
+    }
+}
