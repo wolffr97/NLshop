8001e2b97028af59c95537499d335d6ad9716fab
Nghĩa Lê <lenghia1098@gmail.com>
2020-05-13 16:43:33 +0700

    fix bugs giao dien nguoi dung

--- NLshop.Web/Controllers/HomeController.cs
+++ NLshop.Web/Controllers/HomeController.cs
@@ -26,5 +26,23 @@ namespace NLshop.Web.Controllers
 
             return View();
         }
+
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Header()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Category()
+        {
+            return PartialView();
+        }
     }
 }
